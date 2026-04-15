9b4c0e14b485eb5104bb668b4648252e4d6a9d0e
GONZALEZ <lgonzale@clinic.cat>
2026-04-15 14:54:15 +0200

    Notas 15/04/2026 Seguimos con estadística descriptiva. Preparar estadística de actividad asistencial e ingresos

--- scripts/script_inicial_atdom.R
+++ scripts/script_inicial_atdom.R
@@ -3,7 +3,7 @@ library(compareGroups)
 library(labelled)
 
 # Cargar el diccionario de metadatos desde csv
-metadata_dict <- read_csv("metadata_dict.csv")
+metadata_dict <- read_csv2("metadata_dict.csv")
 
 # Cargar funciones de ayuda
 source("scripts/utils_functions.R")
@@ -12,9 +12,17 @@ source("scripts/utils_functions.R")
 load("data/DF_work.RData")
 load("data/DF_work2.RData")
 
+names(DF_work_2)
+###Añadir SITUACIÓ_2026 es "D" o "A"
+#### añadir varaibles de 104 a 107  
+#recode en otra variable "ALTA_UCIES_num"+"CUAP_num" Si son NA imputar 0 
+###hacer tablas descriptivas similar a descriptiva_strat_2 y descriptiva_strat_3
+
+
 df <- DF_work |>
   as_tibble() |>
-  left_join(DF_work_2 |> select(ID, C_GMA_N_CRONIQUES, VC_VIU_SOL_VALOR, VC_ADEQ_LLAR_VALOR), by = "ID")
+  left_join(DF_work_2 |> select(ID, C_GMA_N_CRONIQUES, VC_VIU_SOL_VALOR, VC_ADEQ_LLAR_VALOR),
+            by = "ID")
 
 # Flujo
 validate_input_data(df, metadata_dict) # valida y lanza warnings/errors
@@ -30,8 +38,13 @@ method <- c(
   coc_nurse = 2,
   DOMICILI_MF_TOT = 2,
   TOTAL_VISITS_MF = 2,
-  coc_physician = 2
+  coc_physician = 2,
+  DOMICILI_CONJ= 2,
+  TOTAL_VISITS_CONJF= 2,
+  coc_conj= 2  
 )
+
+#Descritiva todos los pa. Atdom
 
 descriptiva <- descrTable(
   ~ . - ID,
@@ -43,22 +56,29 @@ descriptiva <- descrTable(
 )
 export2md(descriptiva, format = "html")
 
+#Descritiva en función PHC center
 
-# descriptiva_strat_1 <- descrTable(
-#   USUA_UAB_UP ~ . -ID -organit_atdom_1 -organit_atdom_2,
-#   data = df,
-#   method = method,
-#   hide.no = "no",
-#   include.miss = T,
-#   extra.labels = c("","","","")
-# )
-# export2md(descriptiva_strat_1, format="html")
-# number of groups must be less or equal to 5
 
+descriptiva_strat_1 <- descrTable(
+   USUA_UAB_UP ~ . -ID -organit_atdom_1 -organit_atdom_2,
+   data = df,
+   max.ylev = 7,
+   show.all=T,
+   chisq.test.perm=T,
+   method = method,
+   hide.no = "no",
+   include.miss = T,
+   extra.labels = c("","","","")
+ )
+ export2md(descriptiva_strat_1, format="html")
+
+ #Descritiva en función Equip_Atdom, Equip_Inf	y  UAB_consulta
 
 descriptiva_strat_2 <- descrTable(
   organit_atdom_1 ~ . - ID - USUA_UAB_UP - organit_atdom_2,
   data = df,
+  show.all=T,
+  chisq.test.perm=T,
   method = method,
   hide.no = "no",
   include.miss = T,
@@ -66,10 +86,13 @@ descriptiva_strat_2 <- descrTable(
 )
 export2md(descriptiva_strat_2, format = "html")
 
+#Descritiva Equip_Atdom, Equip_Inf,  UAB_consulta y UAB_consulta_reforç
 
 descriptiva_strat_3 <- descrTable(
   organit_atdom_2 ~ . - ID - USUA_UAB_UP - organit_atdom_1,
   data = df,
+  show.all=T,
+  chisq.test.perm=T,
   method = method,
   hide.no = "no",
   include.miss = T,
