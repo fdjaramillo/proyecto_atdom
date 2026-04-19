8a80d8bb8f43731038a88f1a2c52ad6156ee0a8d
David Jaramillo <fd.jaramilloe@gmail.com>
2026-04-19 23:32:33 +0200

    style: usar styler para facilitar lectura

--- scripts/script_inicial_atdom.R
+++ scripts/script_inicial_atdom.R
@@ -22,7 +22,8 @@ names(DF_work_2)
 df <- DF_work |>
   as_tibble() |>
   left_join(DF_work_2 |> select(ID, C_GMA_N_CRONIQUES, VC_VIU_SOL_VALOR, VC_ADEQ_LLAR_VALOR),
-            by = "ID")
+    by = "ID"
+  )
 
 # Flujo
 validate_input_data(df, metadata_dict) # valida y lanza warnings/errors
@@ -39,12 +40,12 @@ method <- c(
   DOMICILI_MF_TOT = 2,
   TOTAL_VISITS_MF = 2,
   coc_physician = 2,
-  DOMICILI_CONJ= 2,
-  TOTAL_VISITS_CONJF= 2,
-  coc_conj= 2  
+  DOMICILI_CONJ = 2,
+  TOTAL_VISITS_CONJF = 2,
+  coc_conj = 2
 )
 
-#Descritiva todos los pa. Atdom
+# Descritiva todos los pa. Atdom
 
 descriptiva <- descrTable(
   ~ . - ID,
@@ -56,29 +57,28 @@ descriptiva <- descrTable(
 )
 export2md(descriptiva, format = "html")
 
-#Descritiva en función PHC center
-
+# Descritiva en función PHC center
 
 descriptiva_strat_1 <- descrTable(
-   USUA_UAB_UP ~ . -ID -organit_atdom_1 -organit_atdom_2,
-   data = df,
-   max.ylev = 7,
-   show.all=T,
-   chisq.test.perm=T,
-   method = method,
-   hide.no = "no",
-   include.miss = T,
-   extra.labels = c("","","","")
- )
- export2md(descriptiva_strat_1, format="html")
+  USUA_UAB_UP ~ . - ID - organit_atdom_1 - organit_atdom_2,
+  data = df,
+  max.ylev = 7,
+  show.all = T,
+  chisq.test.perm = T,
+  method = method,
+  hide.no = "no",
+  include.miss = T,
+  extra.labels = c("", "", "", "")
+)
+export2md(descriptiva_strat_1, format = "html")
 
- #Descritiva en función Equip_Atdom, Equip_Inf	y  UAB_consulta
+# Descritiva en función Equip_Atdom, Equip_Inf	y  UAB_consulta
 
 descriptiva_strat_2 <- descrTable(
   organit_atdom_1 ~ . - ID - USUA_UAB_UP - organit_atdom_2,
   data = df,
-  show.all=T,
-  chisq.test.perm=T,
+  show.all = T,
+  chisq.test.perm = T,
   method = method,
   hide.no = "no",
   include.miss = T,
@@ -86,13 +86,13 @@ descriptiva_strat_2 <- descrTable(
 )
 export2md(descriptiva_strat_2, format = "html")
 
-#Descritiva Equip_Atdom, Equip_Inf,  UAB_consulta y UAB_consulta_reforç
+# Descritiva Equip_Atdom, Equip_Inf,  UAB_consulta y UAB_consulta_reforç
 
 descriptiva_strat_3 <- descrTable(
   organit_atdom_2 ~ . - ID - USUA_UAB_UP - organit_atdom_1,
   data = df,
-  show.all=T,
-  chisq.test.perm=T,
+  show.all = T,
+  chisq.test.perm = T,
   method = method,
   hide.no = "no",
   include.miss = T,
