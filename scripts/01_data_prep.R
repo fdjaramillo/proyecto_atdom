de55dde516e50007e52b90e7029c408f7bd3404b
David Jaramillo <fd.jaramilloe@gmail.com>
2026-06-30 11:31:40 +0200

    refactor: mover ejecución de descarga espacial a 01_data_prep.R
    Remueve el bloque de código (GIS/sf) de las utilidades. Ahora se ejecuta dentro del flujo de preparación de datos.

--- scripts/01_data_prep.R
+++ scripts/01_data_prep.R
@@ -39,3 +39,36 @@ df <- set_names_to_df(df, metadata_dict) # poner etiquetas
 dir.create("data/processed", showWarnings = FALSE, recursive = TRUE)
 saveRDS(df, "data/processed/df_cleaned.rds")
 message("Dataset guardado exitosamente en data/processed/df_cleaned.rds")
+
+###SF DATA
+library(sf)
+Adreces<- "https://opendata-ajuntament.barcelona.cat/data/dataset/25752522-3528-4c14-b68d-5f09a3e393bd/resource/661fe190-67c8-423a-b8eb-8140f547fde2/download"
+
+download.file(
+  url = Adreces,
+  destfile = "data/adreces.csv",
+  mode = "wb"
+)
+
+BCN_adreces<- st_read("data/adreces.csv")
+
+BCN_adreces<- BCN_adreces %>%
+  mutate(
+    x_etrs89 = na_if(x_etrs89, ""),
+    y_etrs89 = na_if(y_etrs89, ""),
+    x_etrs89 = as.numeric(x_etrs89),
+    y_etrs89 = as.numeric(y_etrs89)
+  ) %>%
+  filter(
+    !is.na(x_etrs89),
+    !is.na(y_etrs89)
+  )%>%
+  st_as_sf(
+    .,
+    coords = c("x_etrs89", "y_etrs89"),
+    crs = 25831,
+    remove = FALSE
+  )
+
+BCN_adreces<-BCN_adreces %>%
+  mutate(nom_carrer=toupper(nom_carrer))
