c93b141f94c0e763313eb7408fe235df194209a0
David Jaramillo <fd.jaramilloe@gmail.com>
2026-07-08 15:50:26 +0200

    refactor: eliminar lineas redundantes en comparación con 01_data_prep

--- scripts/05_Data_SF_downlo_prepr.R
+++ scripts/05_Data_SF_downlo_prepr.R
@@ -1,117 +1,24 @@
 
-# ============================================================
 # 05_SF DATA DOWLOAD AND PREPARATION.R
-# ============================================================
 
-source(here("scripts", "00_setup.R"))
 
 locations<-readRDS(here("data","processed","adreces_SF.rds"))
 
 # Download Adreces ajuntament de Barcelons SF object
 
-Adreces<- "https://opendata-ajuntament.barcelona.cat/data/dataset/25752522-3528-4c14-b68d-5f09a3e393bd/resource/661fe190-67c8-423a-b8eb-8140f547fde2/download"
 
-download.file(
-  url = Adreces,
-  destfile = "data/adreces.csv",
-  mode = "wb"
-)
-
-BCN_adreces<- st_read("data/adreces.csv")
 
 ## Problema amb UTS sense coordanades (borro)
 
-BCN_adreces<- BCN_adreces %>%
-  mutate(
-    x_etrs89 = na_if(x_etrs89, ""),
-    y_etrs89 = na_if(y_etrs89, ""),
-    x_etrs89 = as.numeric(x_etrs89),
-    y_etrs89 = as.numeric(y_etrs89)
-  ) %>%
-  filter(
-    !is.na(x_etrs89),
-    !is.na(y_etrs89)
-  )%>%
-  st_as_sf(
-    .,
-    coords = c("x_etrs89", "y_etrs89"),
-    crs = 25831,
-    remove = FALSE
-  )
 
 # Canvi a majuscules
 
-BCN_adreces<-BCN_adreces %>%
-  mutate(nom_carrer=toupper(nom_carrer))
-
-BCN_adreces<-BCN_adreces %>%
-  mutate(
-    USUA_NUMERO = as.integer(str_remove(numpost_i, "^0+"))
-  )
 
 # Merge amb pacients
 
-User_adreces<- read.csv2(here("data", "external", "USER_adreces_original.csv"),
-                         stringsAsFactors = FALSE
-)
-
-BCN_adreces_users_SF <- BCN_adreces  %>%
-  inner_join(
-    User_adreces[,1:3],
-    by = c(
-      "nom_carrer" = "USUA_CARRER",
-      "USUA_NUMERO" = "USUA_NUMERO"
-    )
-  )
-
-saveRDS(
-  BCN_adreces_users_SF,
-  here("data", "processed", "adreces_SF.rds")
-)
 
 ## Renta media por hogar unidad censal data_frame
 
-Renta_media<- read.csv2(here("data", "external", "renta_media_hogar.csv"),
-                        stringsAsFactors = FALSE
-)
-
-Renta_media <- Renta_media %>%
-  mutate(
-    Seccion.Censal = str_pad(
-      as.character(Seccion.Censal),
-      width = 3,
-      side = "left",
-      pad = "0"
-    ),
-    Distrito = str_pad(
-      as.character(Distrito),
-      width = 2,
-      side = "left",
-      pad = "0"
-    )
-  )  
-
-BCN_adreces_users__renda_SF <- BCN_adreces_users_SF  %>%
-  inner_join(
-    Renta_media[,4:5],
-    by = c(
-      "secc_cens" = "Seccion.Censal"
-    )
-  )
-
-BCN_adreces_users__renda_SF <- BCN_adreces_users_SF %>%
-  left_join(
-    Renta_media,
-    by = c(
-      "districte" = "Distrito",
-      "secc_cens" = "Seccion.Censal"
-    )
-  )
-
-saveRDS(
-  BCN_adreces_users__renda_SF,
-  here("data", "processed", "adreces_SF_Renda.rds")
-)
 
 # Coordenades Centres
 
