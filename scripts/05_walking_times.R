8f78b3e7cfaa51396a8bc078b327b3db18bdab12
Luis González <gonzalezdepaz@users.noreply.github.com>
2026-07-08 07:31:18 +0200

    ff

--- scripts/05_walking_times.R
+++ scripts/05_walking_times.R
@@ -5,6 +5,9 @@ source(here("scripts", "00_setup.R"))
 
 Patients_locations<-readRDS(here("data","processed","pacients_adreces_i_centre_sf.rds"))
 
+Patients_locations<-readRDS(here("data","processed","pacients_adreces_i_centre_sf.rds"))
+
+
 ors_api_key("eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjgzMjA5MjM5MDM4YjRlNzVhZWEyNjgzNmZlYTJjNzFkIiwiaCI6Im11cm11cjY0In0=")
 
 
